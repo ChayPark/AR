4403581d8407dda2ddc0f845f1249e87290f4d15
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-05-25 22:48:42 -0700

    ball weight

--- Assets/Scripts/keypad.cs
+++ Assets/Scripts/keypad.cs
@@ -2,129 +2,88 @@
 using System.Collections.Generic;
 using UnityEngine.UI;
 using UnityEngine;
-using UnityEngine.SceneManagement;
+using TMPro;
 
 public class keypad : MonoBehaviour
 {
-    // Object to be enabled is the keypad. This is needed
-    public GameObject objectToEnable;
+    // Object to be enabled is the keypad.
+    public TMP_InputField charHolder;
+    public GameObject button0;
+    public GameObject button1;
+    public GameObject button2;
+    public GameObject button3;
+    public GameObject button4;
+    public GameObject button5;
+    public GameObject button6;
+    public GameObject button7;
+    public GameObject button8;
+    public GameObject button9;
+    public GameObject flo;
+    public GameObject clear;
+    public GameObject enter;
+   
+    public GameObject bowlingBall;
 
-    // *** Breakdown of header(public) variables *** \\
-    // curPassword : Pasword to set. Ive set the password in the project. Note it can be any length and letters or numbers or sysmbols
-    // input: What is currently entered
-    // displayText : Text area on keypad the password entered gets displayed too.
-    // audioData : Play this sound when user enters in password incorrectly too many times
-
-    [Header("Keypad Settings")]
-    public string curPassword = "123";
-    public string input;
-    public Text displayText;
-    public AudioSource audioData;
-
-    //Local private variables
-    private bool keypadScreen;
-    private float btnClicked = 0;
-    private float numOfGuesses;
-
-    // Start is called before the first frame update
-    void Start()
+    public void b1()
     {
-        btnClicked = 0; // No of times the button was clicked
-        numOfGuesses = curPassword.Length; // Set the password length.
+        charHolder.text = charHolder.text + "1";
     }
-
-    // Update is called once per frame
-    void Update()
+    public void b2()
     {
-        if (btnClicked == numOfGuesses)
-        {
-            if (input == curPassword)
-            {
-                //Load the next scene
-                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    
-                // LOG message that password is correct
-                Debug.Log("Correct Password!");
-                input = ""; //Clear Password
-                btnClicked = 0;
-
-            }
-            else
-            {
-                //Reset input varible
-                input = "";
-                displayText.text = input.ToString();
-                audioData.Play();
-                btnClicked = 0;
-            }
-
-        }
-
+        charHolder.text = charHolder.text + "2";
     }
-
-    void OnGUI()
+    public void b3()
     {
-        // Action for clicking keypad( GameObject ) on screen
-        if (Input.GetMouseButtonDown(0))
-        {
-            RaycastHit hit;
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-
-            if (Physics.Raycast(ray, out hit, 100.0f))
-            {
-                var selection = hit.transform;
-
-                if (selection.CompareTag("keypad")) // Tag on the gameobject - Note the gameobject also needs a box collider
-                {
-                    keypadScreen = true;
-
-                    var selectionRender = selection.GetComponent<Renderer>();
-                    if (selectionRender != null)
-                    {
-                        keypadScreen = true;
-                    }
-                }
-
-            }
-        }
-
-        // Disable sections when keypadScreen is set to true
-        if (keypadScreen)
-        {
-            objectToDisable.SetActive(false);
-            objectToDisable2.SetActive(false);
-            objectToEnable.SetActive(true);
-        }
-
+        charHolder.text = charHolder.text + "3";
     }
-
-    public void ValueEntered(string valueEntered)
+    public void b4()
     {
-        switch (valueEntered)
+        charHolder.text = charHolder.text + "4";
+    }
+    public void b5()
+    {
+        charHolder.text = charHolder.text + "5";
+    }
+    public void b6()
+    {
+        charHolder.text = charHolder.text + "6";
+    }
+    public void b7()
+    {
+        charHolder.text = charHolder.text + "7";
+    }
+    public void b8()
+    {
+        charHolder.text = charHolder.text + "8";
+    }
+    public void b9()
+    {
+        charHolder.text = charHolder.text + "9";
+    }
+    public void b0()
+    {
+        charHolder.text = charHolder.text + "0";
+    }
+    public void bfloat()
+    {
+        charHolder.text = charHolder.text + ".";
+    }
+    public void clearEvent()
+    {
+        charHolder.text = null;
+    }
+    public void enterEvent()
+    {
+        // Check if the text can be parsed to a float
+        if (float.TryParse(charHolder.text, out float weight))
         {
-            case "Q": // QUIT
-                objectToDisable.SetActive(true);
-                objectToDisable2.SetActive(true);
-                objectToEnable.SetActive(false);
-                btnClicked = 0;
-                keypadScreen = false;
-                input = "";
-                displayText.text = input.ToString();
-                break;
-
-            case "C": //CLEAR
-                input = "";
-                btnClicked = 0;// Clear Guess Count
-                displayText.text = input.ToString();
-                break;
-
-            default: // Buton clicked add a variable
-                btnClicked++; // Add a guess
-                input += valueEntered;
-                displayText.text = input.ToString();
-                break;
+            // Change the mass of the bowlingBall object
+            bowlingBall.GetComponent<Rigidbody>().mass = weight;
+            Debug.Log("Weight set to " + weight.ToString() + " kg");
         }
-
-
+        else
+        {
+            Debug.Log("Invalid input. Please enter a valid number for the weight.");
+        }
     }
 }
