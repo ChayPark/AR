ee83653c864d1ef2958fdaab1c49d04d9cd7cce3
ChayPark <99235173+ChayPark@users.noreply.github.com>
2023-05-26 12:36:40 -0700

    fixed reset button

--- Assets/Scripts/RestartButton.cs
+++ Assets/Scripts/RestartButton.cs
@@ -5,12 +5,64 @@ using UnityEngine.SceneManagement;
 
 public class RestartButton : MonoBehaviour
 {
-    public void RestartGame(string sceneName)
+    public GameObject pin1;
+    public GameObject pin2;
+    public GameObject pin3;
+    public GameObject pin4;
+    public GameObject pin5;
+    public GameObject pin6;
+    public GameObject pin7;
+    public GameObject pin8;
+    public GameObject pin9;
+    public GameObject pin10;
+
+    public GameObject ball;
+
+    public Vector3 initialPin1Position;
+    public Vector3 initialPin2Position;
+    public Vector3 initialPin3Position;
+    public Vector3 initialPin4Position;
+    public Vector3 initialPin5Position;
+    public Vector3 initialPin6Position;
+    public Vector3 initialPin7Position;
+    public Vector3 initialPin8Position;
+    public Vector3 initialPin9Position;
+    public Vector3 initialPin10Position;
+
+    public Vector3 initialBallPosition;
+
+    private void Start()
     {
-        SceneManager.LoadScene(sceneName);
+        // Save initial positions
+        initialPin1Position = pin1.transform.position;
+        initialPin2Position = pin2.transform.position;
+        initialPin3Position = pin3.transform.position;
+        initialPin4Position = pin4.transform.position;
+        initialPin5Position = pin5.transform.position;
+        initialPin6Position = pin6.transform.position;
+        initialPin7Position = pin7.transform.position;
+        initialPin8Position = pin8.transform.position;
+        initialPin9Position = pin9.transform.position;
+        initialPin10Position = pin10.transform.position;
+
+        initialBallPosition = ball.transform.position;
     }
-    public void ReloadCurrenrtScene()
+
+    public void ResetPositions()
     {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        // Reset pin positions
+        pin1.transform.position = initialPin1Position;
+        pin2.transform.position = initialPin2Position;
+        pin3.transform.position = initialPin3Position;
+        pin4.transform.position = initialPin4Position;
+        pin5.transform.position = initialPin5Position;
+        pin6.transform.position = initialPin6Position;
+        pin7.transform.position = initialPin7Position;
+        pin8.transform.position = initialPin8Position;
+        pin9.transform.position = initialPin9Position;
+        pin10.transform.position = initialPin10Position;
+
+        // Reset ball position
+        ball.transform.position = initialBallPosition;
     }
 }
